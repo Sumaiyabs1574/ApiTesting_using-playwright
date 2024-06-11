6a1577e5142178a01d000cf75b79af550921b65a
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-06-11 18:33:48 +0600

    Create POST api request

---
+++ tests/post_api_request_01.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+test("Create post api request using static request body", async ({request}) => {
+
+const postApiResponse = await request.post('/booking',{
+
+data:{
+
+    
+        "firstname": "sumaiya",
+        "lastname": "habib",
+        "totalprice": 1000,
+        "depositpaid": true,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "super bowls"
+    
+}
+
+
+})
+  const postApiResponseBody = await postApiResponse.json();
+console.log(postApiResponseBody);
+
+})

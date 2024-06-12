6bad85e21f4009a20c058242485dc166fff3d1fd
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-06-12 18:59:50 +0600

    Get api request

---
+++ tests/get_api_ request_04.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+const bookingApiRequestBody=require ("../test_data/post_request_body.json");
+test("Create get api request", async ({request}) => {
+
+const getApiResponse = await request.post('/booking',{
+
+data:bookingApiRequestBody
+
+})
+ const bid=getApiResponse.bookingid
+
+
+ const getApiResponseBody= await request.get('/booking/${bid}');
+await console.log(getApiResponseBody.json)
+
+})

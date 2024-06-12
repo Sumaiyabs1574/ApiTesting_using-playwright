572f011e44d0cc621229e7abb29e95ba1f153c1a
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-06-12 14:58:46 +0600

    geting data from jeson file

--- tests/post_api_request_02.spec.js
+++ tests/post_api_request_02.spec.js
@@ -1,25 +1,13 @@
 import { test, expect } from "@playwright/test";
+const bookingApiRequestBody=require ("../test_data/post_request_body.json");
 test("Create post api request using static request body", async ({request}) => {
 
 const postApiResponse = await request.post('/booking',{
 
-data:{
-
-    
-        "firstname": "sumaiya",
-        "lastname": "habib",
-        "totalprice": 1000,
-        "depositpaid": true,
-        "bookingdates": {
-            "checkin": "2018-01-01",
-            "checkout": "2019-01-01"
-        },
-        "additionalneeds": "super bowls"
-    
-}
-
+data:bookingApiRequestBody
 
 })
+
 //validate status code
 expect (postApiResponse.ok()).toBeTruthy();
 expect (postApiResponse.status()).toBe(200);
